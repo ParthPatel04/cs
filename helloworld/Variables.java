69eb4858120980fc54b240c1ad948486f08b9a58
ParthPatel04 <parthmpatel04@gmail.com>
2017-07-18 18:29:42 -0400

    Learned about Variables

---
+++ helloworld/Variables.java
@@ -0,0 +1,18 @@
+public class Variables{
+	public static void main (String args[]){
+		int a = 4;
+		System.out.println(a);
+
+		char b = 'P';
+		System.out.println(b);
+
+		long c = 2934543440L;
+		System.out.println(c);
+
+		String d = "Parth Vader Rules, Now Think Again, MASTER!";
+		System.out.println(d);
+
+		float e = 3.1F;
+		System.out.println(e);
+	}
+}
