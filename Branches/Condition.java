ead51fdb0d76c08e4c12a6baf79f52f3c3290c57
ParthPatel04 <parthmpatel04@gmail.com>
2017-09-13 17:43:26 -0400

    Added Condition

---
+++ Branches/Condition.java
@@ -0,0 +1,22 @@
+public class Condition{
+	public static void main(String[] args) {
+
+		int num = 15;
+
+		if (num == 15) {
+			System.out.println("Smelly Stink Stink");
+		}
+
+		if (num % 5 == 0) {
+			System.out.println("Hi Ya Hi YA");
+		}
+
+		if (num % 3 == 0|| num == 22) {
+			System.out.println("I am a Beaver");
+		}
+
+		if (num % 3 == 0 && num % 5 == 0) {
+			System.out.println("Bingo Is His Name Yo!!!");
+		}
+ 	}
+}
